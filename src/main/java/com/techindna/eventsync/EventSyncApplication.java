bfade4a9daa7ce4c86f81a37ea9f1f3eff758899
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-01 10:49:38 +0300

    fix(security): using .env file in application.properties

--- src/main/java/com/techindna/eventsync/EventSyncApplication.java
+++ src/main/java/com/techindna/eventsync/EventSyncApplication.java
@@ -2,7 +2,6 @@ package com.techindna.eventsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class EventSyncApplication {
--- src/main/java/com/techindna/eventsync/controller/AuthController.java
+++ src/main/java/com/techindna/eventsync/controller/AuthController.java
@@ -8,8 +8,6 @@ import com.techindna.eventsync.exception.InternalServerErrorException;
 import com.techindna.eventsync.exception.UnauthorizedException;
 import com.techindna.eventsync.service.AuthService;
 import com.techindna.eventsync.validator.AuthValidator;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
@@ -19,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
