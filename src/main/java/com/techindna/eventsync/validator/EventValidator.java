fb1f7bc55c4b2b32c9e18d1e605f02250e57f1d8
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-02 11:17:40 +0300

    fix(POST /events): checking either the date is null, blank or empty

--- src/main/java/com/techindna/eventsync/validator/EventValidator.java
+++ src/main/java/com/techindna/eventsync/validator/EventValidator.java
@@ -3,6 +3,7 @@ package com.techindna.eventsync.validator;
 import com.techindna.eventsync.exception.BadRequestException;
 import org.springframework.stereotype.Component;
 
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,20 +14,34 @@ public class EventValidator {
 
     private void validate(String fieldName, String data){
         if (data == null || data.isEmpty() || data.isBlank()){
-            throw new BadRequestException(String.format("The field %s is required and cannot be blank", fieldName));
+            throw new BadRequestException(String.format("The field %s is required and cannot be blank.", fieldName));
         }
 
         final Matcher TEXT_MATCHER = TEXT_PATTERN.matcher(data);
         if (!TEXT_MATCHER.matches()){
-            throw new BadRequestException(String.format("Invalid input for %s: only a-zA-Z0-9 .,':- characters are allowed", data));
+            throw new BadRequestException(String.format("Invalid input for %s: %s only a-zA-Z0-9 .,':- characters are allowed.", fieldName, data));
         }
+    }
+
+    private void  validateDate(String fieldName, Instant date){
+        if (date == null || date.toString().isBlank() || date.toString().isEmpty()){
+            throw new BadRequestException(String.format("The field %s is required and cannot be blank.", fieldName));
+        }
+
+        /* Didn't pass test 13
+        try {
+            Instant.parse(String.valueOf(date));
+        }catch (DateTimeException e){
+            throw new BadRequestException(String.format("The field %s contain forbidden character or is invalid.", fieldName));
+        }
+         */
     }
 
     public void  validateEventData(String title, String description, Instant startDate, Instant endDate, String location){
         validate("title", title);
         validate("description", description);
-        validate("startDate", String.valueOf(startDate));
-        validate("endDate", String.valueOf(endDate));
+        validateDate("startDate", startDate);
+        validateDate("endDate", endDate);
         validate("location", location);
     }
 }
