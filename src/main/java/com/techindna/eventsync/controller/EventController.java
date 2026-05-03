5daba8bc65b0da0094b1dbb03fdeb812b581c004
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-03 15:56:23 +0300

    fix: response message

--- src/main/java/com/techindna/eventsync/controller/EventController.java
+++ src/main/java/com/techindna/eventsync/controller/EventController.java
@@ -141,7 +141,7 @@ public class EventController {
         try {
             UuidValidator.validateUUID(id);
             UUID deletedId = eventService.deleteEvent(UUID.fromString(id));
-            return ResponseEntity.status(HttpStatus.OK).body(deletedId);
+            return ResponseEntity.status(HttpStatus.OK).body(String.format("Event %s deleted", deletedId));
         } catch (BadRequestException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(e.getMessage());
