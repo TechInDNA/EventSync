289761790d94eeb90d9ddf12a166147faa403041
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-13 09:50:31 +0300

    fix: return SessionResponseDto instead of Optional from createSession

--- src/main/java/com/techindna/eventsync/service/SessionService.java
+++ src/main/java/com/techindna/eventsync/service/SessionService.java
@@ -2,6 +2,7 @@ package com.techindna.eventsync.service;
 
 import com.techindna.eventsync.dto.SessionRequestDto;
 import com.techindna.eventsync.dto.SessionResponseDto;
+import com.techindna.eventsync.exception.ConflictException;
 import com.techindna.eventsync.exception.NotFoundException;
 import com.techindna.eventsync.repository.SessionRepository;
 import com.techindna.eventsync.validator.DataValidator;
@@ -25,11 +26,12 @@ public class SessionService {
                 sessionRequestDto.getDescription(),
                 String.valueOf(sessionRequestDto.getStartDate()),
                 String.valueOf(sessionRequestDto.getEndDate()),
-                String.valueOf(sessionRequestDto.getRoomName()),
-                String.valueOf(sessionRequestDto.getEventTitle()),
+                sessionRequestDto.getRoomName(),
+                sessionRequestDto.getEventTitle(),
                 sessionRequestDto.getCapacity()
         );
-        return sessionRepository.createSession(sessionRequestDto);
+        return sessionRepository.createSession(sessionRequestDto)
+                .orElseThrow(() -> new ConflictException(String.format("Session with title '%s' already exists", sessionRequestDto.getTitle())));
     }
 
     public UUID deleteSessionById(UUID id) {
