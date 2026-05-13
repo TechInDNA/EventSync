390a761f5221652cf9733e746da5640d5d6a7a31
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-13 10:47:22 +0300

    fix: add JsonProperty isLive to match API spec field name

--- src/main/java/com/techindna/eventsync/dto/SessionResponseDto.java
+++ src/main/java/com/techindna/eventsync/dto/SessionResponseDto.java
@@ -1,10 +1,12 @@
 package com.techindna.eventsync.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.techindna.eventsync.entity.Session;
 
 public class SessionResponseDto extends Session {
     private boolean isLive;
 
+    @JsonProperty("isLive")
     public boolean isLive() {
         return isLive;
     }
