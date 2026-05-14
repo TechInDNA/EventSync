3a604800afb8cf0cfdb4a2fde3fbefc78671ff94
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-14 15:04:15 +0300

    fix: correct SQL syntax and parameter binding in getAllSessions query

--- src/main/java/com/techindna/eventsync/repository/SessionRepository.java
+++ src/main/java/com/techindna/eventsync/repository/SessionRepository.java
@@ -1,7 +1,6 @@
 package com.techindna.eventsync.repository;
 
-import com.techindna.eventsync.dto.SessionRequestDto;
-import com.techindna.eventsync.dto.SessionResponseDto;
+import com.techindna.eventsync.dto.*;
 import com.techindna.eventsync.entity.Event;
 import com.techindna.eventsync.entity.Room;
 import com.techindna.eventsync.entity.Session;
@@ -14,6 +13,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,6 +29,160 @@ public class SessionRepository {
         this.dataSource = dataSource;
         this.roomRepository = roomRepository;
         this.eventRepository = eventRepository;
+    }
+
+    private String getAllSessionsQueryHandler(GetSessionRequestDto request){
+        final StringBuilder query = new StringBuilder(
+                """
+                select
+                sessions.id as session_id,
+                sessions.title as session_title,
+                sessions.description as session_description,
+                sessions.start_date as session_start_date,
+                sessions.end_date as session_end_date,
+                rooms.id as room_id,
+                rooms.name as room_name,
+                sessions.capacity,
+                events.id as event_id,
+                events.title as event_title,
+                events.description as event_description,
+                events.start_date as event_start_date,
+                events.end_date as event_end_date,
+                events.location,
+                events.created_at
+                from eventsync_app.intervene
+                join eventsync_app.sessions
+                on intervene.session_id = sessions.id
+                join eventsync_app.users
+                on intervene.speaker_id = users.id
+                join eventsync_app.rooms
+                on sessions.room_id = rooms.id
+                join eventsync_app.events
+                on events.id = sessions.event_id
+                where users."role" = 'speaker'
+                """);
+        if (request.getEventTitle() != null) {
+            query.append(" and events.title ilike ?");
+        }
+        if (request.getSpeakerName() != null) {
+            query.append(" and (users.first_name ilike ? or users.last_name ilike ?)");
+        }
+        if (request.getRoomName() != null) {
+            query.append(" and rooms.name ilike ?");
+        }
+        if (request.isLive()){
+            query.append(" ? between sessions.start_date and sessions.end_date");
+        }
+        query.append(" limit ? offset ?");
+        return String.valueOf(query);
+    }
+
+    public int countSession(){
+        final String query = "select count(sessions.id) as total from eventsync_app.sessions";
+        try(
+                Connection connection = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query)
+        ){
+            try (ResultSet rs = ps.executeQuery()){
+                return rs.next() ? rs.getInt("total") : 0;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<SpeakerInterventionDto> getInterventionById(UUID sessionId){
+        final String query =
+                """
+                select
+                users.first_name,
+                users.last_name,
+                users.profile_picture,
+                users.bio
+                from eventsync_app.intervene
+                join eventsync_app.users
+                on users.id = intervene.speaker_id
+                where intervene.session_id = ?
+                """;
+        final List<SpeakerInterventionDto> interventions = new ArrayList<>();
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query)
+        ){
+            ps.setObject(1,sessionId);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    SpeakerInterventionDto intervention = new SpeakerInterventionDto();
+                    intervention.setFirstName(rs.getString("first_name"));
+                    intervention.setLastName(rs.getString("last_name"));
+                    intervention.setProfilePicture(rs.getString("profile_picture"));
+                    intervention.setBio(rs.getString("bio"));
+                    interventions.add(intervention);
+                }
+                return interventions;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<SessionResponseDto> getAllSessions(GetSessionRequestDto request, PaginationRequestDto pagination){
+        try(
+                Connection connection = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(getAllSessionsQueryHandler(request))
+        ){
+            int paramIndex = 1;
+            if (request.getEventTitle() != null) {
+                ps.setString(paramIndex++, String.format("%%%s%%", request.getEventTitle()));
+            }
+            if (request.getSpeakerName() != null) {
+                ps.setString(paramIndex++, String.format("%%%s%%", request.getSpeakerName()));
+                ps.setString(paramIndex++, String.format("%%%s%%", request.getSpeakerName()));
+            }
+            if (request.getRoomName() != null) {
+                ps.setString(paramIndex++, String.format("%%%s%%", request.getRoomName()));
+            }
+            if (request.isLive()){
+                ps.setTimestamp(paramIndex++, Timestamp.from(ACTUAL_DATE));
+            }
+            ps.setInt(paramIndex++, pagination.getLimit());
+            ps.setInt(paramIndex, pagination.getOffset());
+
+            List<SessionResponseDto> sessions = new ArrayList<>();
+            try (ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                    SessionResponseDto session = new SessionResponseDto();
+                    session.setId(UUID.fromString(rs.getString("session_id")));
+                    session.setTitle(rs.getString("session_title"));
+                    session.setDescription(rs.getString("session_description"));
+                    session.setStartDate(rs.getTimestamp("session_start_date").toInstant());
+                    session.setEndDate(rs.getTimestamp("session_end_date").toInstant());
+                    session.setCapacity(rs.getInt("capacity"));
+
+                    Room room = new Room();
+                    room.setId(UUID.fromString(rs.getString("room_id")));
+                    room.setName(rs.getString("room_name"));
+
+                    session.setRoom(room);
+
+                    Event event = new Event();
+                    event.setId(UUID.fromString(rs.getString("event_id")));
+                    event.setTitle(rs.getString("event_title"));
+                    event.setDescription(rs.getString("event_description"));
+                    event.setStartDate(rs.getTimestamp("event_start_date").toInstant());
+                    event.setEndDate(rs.getTimestamp("event_end_date").toInstant());
+
+                    session.setEvent(event);
+
+                    session.setSpeakers(getInterventionById(session.getId()));
+
+                    sessions.add(session);
+                }
+                return sessions;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Optional<SessionResponseDto> createSession(SessionRequestDto sessionRequestDto) {
