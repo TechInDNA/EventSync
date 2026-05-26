4b922ced7e9145d97da1b76ed7a1043dd900b36d
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-26 16:12:52 +0300

    fix: remove UnauthorizedException catch in SessionController + make delete script executable

--- src/main/java/com/techindna/eventsync/controller/SessionController.java
+++ src/main/java/com/techindna/eventsync/controller/SessionController.java
@@ -142,8 +142,6 @@ public class SessionController {
         } catch (BadRequestException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(e.getMessage());
-        } catch (UnauthorizedException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         } catch (NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(e.getMessage());
