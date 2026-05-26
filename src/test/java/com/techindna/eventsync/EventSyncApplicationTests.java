82e4205be2496c722d5d2bc4d8ab2ac76bc6c296
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-05-26 16:58:08 +0300

    use mock DataSource in test instead of H2 - removes need for any DB or env vars in Poja CI

--- src/test/java/com/techindna/eventsync/EventSyncApplicationTests.java
+++ src/test/java/com/techindna/eventsync/EventSyncApplicationTests.java
@@ -2,12 +2,27 @@ package com.techindna.eventsync;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.mockito.Mockito;
+import javax.sql.DataSource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Import(EventSyncApplicationTests.TestDataSourceConfig.class)
 class EventSyncApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @TestConfiguration
+    static class TestDataSourceConfig {
+        @Bean
+        DataSource dataSource() {
+            return Mockito.mock(DataSource.class);
+        }
     }
 
 }
